a1791eac7fbe6515809a200295497ccf769af484
Napis <nael82200@outlook.fr>
2023-12-04 21:44:14 +0100

    Création d'une interface utilisateur (HomeMenu.cs)

---
+++ CGASHelpProgram/Class/RoundedButton.Designer.cs
@@ -0,0 +1,28 @@
+﻿namespace CGASHelpProgram.Class
+{
+    partial class RoundedButton
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        
+        #region Code généré par le Concepteur de composants
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+        }
+
+        #endregion
+    }
+}
